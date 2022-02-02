aa8d3286591657ca449a0ca0afcb1ccfc904d4e6
nikola2609 <nikolalazovic26@gmail.com>
2022-02-02 16:46:39 +0100

    prijava

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
+require "Database.php";
+require "model/Prijava.php";
 
+$greska="";
+session_start();
+
+if (isset($_SESSION['current_user'])) {
+    header('Location: pocetna.php');
+    exit();
+}
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+
+    $odg=Prijava::prijaviSe($conn,$username,$password);
+    if($odg!=null && $odg->num_rows==1){
+        $red=$odg->fetch_assoc();
+        $_SESSION['current_user']=$red['username'];
+        header('Location: pocetna.php');
+        exit();
+    }else{
+        $greska="Pogrešno korisničko ime ili lozinka!";
+    }
+}
+?>
